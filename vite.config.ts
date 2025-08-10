81f6568ec7b7f515304c9e372dd155a9bed99b33
J0j4t4n <tssecuritys@gmail.com>
2025-08-10 03:39:20 -0500

    Update deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/mascotas/',
+      base: '/MASCOTAS2/',
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
